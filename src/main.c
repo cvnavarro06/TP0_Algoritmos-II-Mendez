6c8cbd88b3e79efe4a4b458b9ff2fd9c6fdfd635
constantino <cvnavarro@fi.uba.ar>
2026-08-26 18:53:28 -0300

    Fix: Adapto el main para las devoluciones de las funciones de la libreria leer_linea.h

--- src/main.c
+++ src/main.c
@@ -6,51 +6,47 @@
 #define ERROR -1
 #define EXIT -2
 
-
 int main(int argc, char *argv[])
 {
 	printf("Ingresa tu nombre: ");
 
 	char *nombre = leer_linea();
 
-    if (nombre == NULL) {
-        printf("Ha ocurrido un error a la hora de solicitar memoria\n\n"
-               "Cerrando programa...");
-        return ERROR;
-    }
-    printf("\nBienvenido %s, ingresa los nombres de los alumnos al sistema\n\n", nombre);
+	if (nombre == NULL) {
+		printf("Ha ocurrido un error a la hora de solicitar memoria\n\n"
+		       "Cerrando programa...");
+		return ERROR;
+	}
+	printf("\nBienvenido %s\nIngresa los nombres de los alumnos al sistema\n\n",
+	       nombre);
 
-    printf("Recueda que para salir del programa debes de ingresar 'Salir'\n\n");
+	printf("Recueda que para salir del programa debes de ingresar 'Salir'\n\n");
 
-    
-    size_t tamanio_l = 10;
-    char *linea = malloc(tamanio_l * sizeof(char));
+	size_t tamanio_l = 10;
+	char *linea = malloc(tamanio_l * sizeof(char));
 
-    printf("Ingresa un nombre: ");
+	printf("Ingresa un nombre: ");
 
-    bool salir = false;
-    
-    int c;
-    
-    while (!salir && (c = leer_linea_ptr(&linea, &tamanio_l)) != ERROR) {
-        
-        if (c == EXIT) {
-            salir = true;
-            printf("Saliendo del programa...\n");
-        } else {
-        
-            printf("%s ha sido ingresado al sistema.\n", linea);
+	bool salir = false;
 
-            printf("El largo del nombre %s es de: %d\n\n", linea, c); 
-        
-            printf("Ingresa otro nombre: ");
-        }
+	int c;
 
-    }
+	while (!salir && (c = leer_linea_ptr(&linea, &tamanio_l)) != ERROR) {
+		if (c == EXIT) {
+			salir = true;
+			printf("Saliendo del programa...\n");
+		} else {
+			printf("Has agregado un nuevo alumno, nombre: %s\n", linea);
 
-    free(linea);
-	
-    free(nombre);
-	
-    return EXITO;
+			printf("El largo del nombre %d\n\n", c);
+
+			printf("Ingresa otro nombre: ");
+		}
+	}
+
+	free(linea);
+
+	free(nombre);
+
+	return EXITO;
 }
